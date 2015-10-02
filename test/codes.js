2c8eeab22bdf0c01bfc7dc2cc1e07a338d8dd5eb
Hannes Moser <hannes@impossiblearts.com>
2015-10-02 16:26:38 +0200

    test: add status code tests for all classes subclassing HTTPError

--- test/codes.js
+++ test/codes.js
@@ -1,35 +1,61 @@
-import * as errors from '../src'
+import {
+  // 4xx
+  BadRequest,
+  Unauthorized,
+  Forbidden,
+  NotFound,
+  MethodNotAllowed,
+  NotAcceptable,
+  ProxyAuthenticationRequired,
+  RequestTimeout,
+  Conflict,
+  Gone,
+  LengthRequired,
+  PreconditionFailed,
+  RequestEntityTooLarge,
+  RequestURITooLong,
+  UnsupportedMediaType,
+  RequestedRangeNotSatisfiable,
+  ExpectationFailed,
+  // 5xx
+  InternalServerError,
+  NotImplemented,
+  BadGateway,
+  ServiceUnavailable,
+  GatewayTimeout,
+  HttpVersionNotSupported
+} from '../src'
 
 describe('errors', () => {
   describe('codes', () => {
 
     it('should compare client error codes', () => {
-      errors.badRequest().code.should.eql(400)
-      errors.unauthorized().code.should.eql(401)
-      errors.forbidden().code.should.eql(403)
-      errors.notFound().code.should.eql(404)
-      errors.methodNotAllowed().code.should.eql(405)
-      errors.notAcceptable().code.should.eql(406)
-      errors.proxyAuthenticationRequired().code.should.eql(407)
-      errors.requestTimeout().code.should.eql(408)
-      errors.conflict().code.should.eql(409)
-      errors.gone().code.should.eql(410)
-      errors.lengthRequired().code.should.eql(411)
-      errors.preconditionFailed().code.should.eql(412)
-      errors.requestEntityTooLarge().code.should.eql(413)
-      errors.requestURITooLong().code.should.eql(414)
-      errors.unsupportedMediaType().code.should.eql(415)
-      errors.requestedRangeNotSatisfiable().code.should.eql(416)
-      errors.expectationFailed().code.should.eql(417)
+      new BadRequest().status.should.eql(400)
+      new Unauthorized().status.should.eql(401)
+      new Forbidden().status.should.eql(403)
+      new NotFound().status.should.eql(404)
+      new MethodNotAllowed().status.should.eql(405)
+      new NotAcceptable().status.should.eql(406)
+      new ProxyAuthenticationRequired().status.should.eql(407)
+      new RequestTimeout().status.should.eql(408)
+      new Conflict().status.should.eql(409)
+      new Gone().status.should.eql(410)
+      new LengthRequired().status.should.eql(411)
+      new PreconditionFailed().status.should.eql(412)
+      new RequestEntityTooLarge().status.should.eql(413)
+      new RequestURITooLong().status.should.eql(414)
+      new UnsupportedMediaType().status.should.eql(415)
+      new RequestedRangeNotSatisfiable().status.should.eql(416)
+      new ExpectationFailed().status.should.eql(417)
     })
 
     it('should compare server error codes', () => {
-      errors.internalServerError().code.should.eql(500)
-      errors.notImplemented().code.should.eql(501)
-      errors.badGateway().code.should.eql(502)
-      errors.serviceUnavailable().code.should.eql(503)
-      errors.gatewayTimeout().code.should.eql(504)
-      errors.httpVersionNotSupported().code.should.eql(505)
+      new InternalServerError().status.should.eql(500)
+      new NotImplemented().status.should.eql(501)
+      new BadGateway().status.should.eql(502)
+      new ServiceUnavailable().status.should.eql(503)
+      new GatewayTimeout().status.should.eql(504)
+      new HttpVersionNotSupported().status.should.eql(505)
     })
 
   })
