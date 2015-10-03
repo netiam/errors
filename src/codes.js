dc2d90fe4178f9cffb699e502e9eaa32b9d38b07
Hannes Moser <hannes@impossiblearts.com>
2015-10-03 17:02:20 +0200

    fix: code types
    Code types should be unique strings and therefore they are defined as
    common string constants now.

--- src/codes.js
+++ src/codes.js
@@ -1,9 +1,14 @@
 export const E1000 = {
   code: 1000,
-  type: 'Generic user error'
+  type: 'GENERIC_USER_ERROR'
 }
 
 export const E2000 = {
   code: 2000,
-  type: 'Generic authentication error'
+  type: 'GENERIC_AUTHENTICATION_ERROR'
+}
+
+export const E3000 = {
+  code: 3000,
+  type: 'GENERIC_AUTHORIZATION_ERROR'
 }
